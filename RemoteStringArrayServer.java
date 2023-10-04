953cc79071d16173e4f65aba24deb3c528a36a96
Bipul Singh <bipul392ssnl@gmail.com>
2023-10-04 13:41:22 -0400

    Chnages in Server and code for Client

--- RemoteStringArrayServer.java
+++ RemoteStringArrayServer.java
@@ -14,64 +14,88 @@ public class RemoteStringArrayServer implements RemoteStringArray {
     }
 
     @Override
+    public int size() {
+        return strArray.length;
+    }
+
+    @Override
     public boolean writeBackElement(String str, int l, int client_id) {
-        // TODO Auto-generated method stub
+
+        if(checkWriteLock(client_id, l)) {
+            strArray[l] = str;
+            return true;
+        }
         return false;
     }
 
-    // Check for the read only lock for the client_id
     @Override
     public String fetchElementRead(int l, int client_id) {
-        if (checkReadLock(client_id, l)) {
+        // If read lock is not present add lock if possible
+        // Write lock can read elements
+        if (requestReadLock(l, client_id) || checkWriteLock(client_id, l)) {
             return strArray[l];
-        } else {
-            throw new RuntimeException("No read Only Lock");
         }
+        // } else {
+        //     throw new RuntimeException("No read Only Lock");
+        // }
+        return null;
     }
 
     // Check Read Lock on element for client_id
     // return true if lock is there
+    // If write lock is there for the client return true
     private boolean checkReadLock(int client_id, int l) {
 
         List<Integer> clientList;
         clientList = readLockMap.get(l);
+        if(checkWriteLock(client_id, l)) return true;
         if (clientList != null) {
             return clientList.contains(client_id);
         }
         return false;
     }
 
-    // Check Read Lock on element 
+    // Check Read Lock on element
     // return true if lock is there
     private boolean checkReadLock(int element) {
-        if(readLockMap.get(element) != null && !readLockMap.get(element).isEmpty())
-            return true;
-        else
-            return false;
+        return readLockMap.get(element) != null && !readLockMap.get(element).isEmpty();
     }
 
-    // Check Read Lock on element 
+    // Check Read Lock on element
     // return true if lock is there
     private boolean checkWriteLock(int element) {
-        if (writeLockMap.get(element) != null) {
-            return true;
+        return writeLockMap.get(element) != null;
+    }
+
+    private boolean checkWriteLock(int client_id, int element) {
+        return checkWriteLock(element) && client_id == writeLockMap.get(element);
+    }
+
+     public boolean checkReadLockPerClient(int client_id, int element) {
+        if(checkReadLock(element)) {
+            return readLockMap.get(element).stream().allMatch(client -> client == client_id);
         }
         return false;
+
     }
 
     @Override
     public String fetchElementWrite(int l, int client_id) {
-        // TODO Auto-generated method stub
+        if(requestWriteLock(l, client_id)) {
+            return strArray[l];
+        }
         return null;
     }
 
     @Override
     public void insertArrayElement(int l, String str) {
-        // TODO - add the checks here
-        strArray[l] = str;
+        if (l >= 0 && l < strArray.length) {
+            strArray[l] = str;
+        }
     }
 
     // Release any read or write lock on the element for that Id
+    // Only client having lock will call it
     @Override
     public void releaseLock(int l, int client_id) {
 
@@ -79,7 +103,20 @@ public class RemoteStringArrayServer implements RemoteStringArray {
             if (writeLockMap.get(l) != l && writeLockMap.get(l) == client_id) {
                 writeLockMap.remove(l);
             }
-            if(readLockMap.get(l) != null) {
+            if (readLockMap.get(l) != null) {
+                readLockMap.get(l).remove(client_id);
+            }
+        } catch (RuntimeException e) {
+
+        }
+
+    }
+
+    @Override
+    public void releaseReadLock(int l, int client_id) {
+
+        try {
+            if (readLockMap.get(l) != null) {
                 readLockMap.get(l).remove(client_id);
             }
         } catch (RuntimeException e) {
@@ -92,20 +129,21 @@ public class RemoteStringArrayServer implements RemoteStringArray {
     public boolean requestReadLock(int l, int client_id) {
         try {
             // If write lock is there then no read lock
-            if (!checkWriteLock(l) && !checkReadLock(client_id, l)) {
-            // TODO - What to return if lock is already present for that client_id
-            if (!readLockMap.containsKey(l)) {
-                readLockMap.put(l, Arrays.asList(client_id));
+            if (checkReadLock(client_id, l)) {
                 return true;
-            } else {
-                readLockMap.get(l).add(client_id);
+            } else if (!checkWriteLock(l)) {
+                if (!readLockMap.containsKey(l)) {
+                    readLockMap.put(l, Arrays.asList(client_id));
+                    return true;
+                } else {
+                    readLockMap.get(l).add(client_id);
+                }
+                return true;
             }
-            return true;
-        }
-            
+
         } catch (RuntimeException e) {
-            // Error occured while obtaining lock
-            return false;
+            System.out.println("Error occured while obtaining read lock for Client: " + client_id +
+                    " elementId: " + l + "Error: " + e.getMessage());
         }
         return false;
     }
@@ -113,7 +151,11 @@ public class RemoteStringArrayServer implements RemoteStringArray {
     // Assing the write lock to client_id
     @Override
     public boolean requestWriteLock(int l, int client_id) {
-        if(!checkReadLock(l) & !checkWriteLock(l)) {
+        if (checkWriteLock(client_id, l))
+            return true;
+        // Release readLock by this client on that element. If there
+        releaseReadLock(l, client_id);
+        if (!checkReadLock(l) & !checkWriteLock(l)) {
             writeLockMap.put(l, client_id);
             return true;
         }
--- StringArrayClient.java
+++ StringArrayClient.java
@@ -1,7 +1,67 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringArrayClient {
 
-    public static void main(String[] args) {
+    private int client_id;
+    private RemoteStringArray stringArrayServer;
+    String currentWriteStr;
+    Map<Integer, String> localStringArrayMapping;
+
+    public StringArrayClient(int client_id) {
+        this.client_id = client_id;
+        localStringArrayMapping = new HashMap<>();
+    }
+
+    public int getArrayCapacity() {
+        return stringArrayServer.size();
+    }
+
+    public String fetchElementRead(int l) {
+        String readStr = null;
+        try {
+             readStr =  stringArrayServer.fetchElementRead(l, client_id);
+             if(readStr != null) {
+                localStringArrayMapping.put(l, readStr);
+             }
+        } catch (Exception e) {
+            System.err.println("Error while Read fetching: "+e.getMessage());
+        }
+        return readStr;
         
     }
+
+    public String fetchElementWrite(int l) {
+        String writeStr = null;
+        try {
+            writeStr = stringArrayServer.fetchElementWrite(l, client_id);
+        } catch (Exception e) {
+            System.err.println("Error while Write fetching: "+e.getMessage());
+        }
+        return writeStr;
+    }
+
+    // Expected that the element is fetched either in read or write mode
+    public String printElement(int l) {
+        return localStringArrayMapping.get(l);
+    }
+
+    public boolean concatenate(int l, String concStr){
+        if(localStringArrayMapping.get(l) != null) {
+            localStringArrayMapping.put(l, localStringArrayMapping.get(l).concat(concStr));
+            return true;
+        }
+        return false;
+    }
+
+    public String writeBack(int l) {
+        if(stringArrayServer.writeBackElement(localStringArrayMapping.get(l), l, client_id))
+            return "Success";
+        else
+            return "Failure";
+        
+    }
+
+
     
 }
