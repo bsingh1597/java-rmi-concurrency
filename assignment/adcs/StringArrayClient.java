96a2e339acea11626547d0bb32867ed7c8aef791
Soumya130498 <113308735+Soumya130498@users.noreply.github.com>
2023-10-08 18:51:56 -0400

    package

--- assignment/adcs/StringArrayClient.java
+++ assignment/adcs/StringArrayClient.java
@@ -8,6 +8,7 @@ import java.rmi.registry.Registry;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.List;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
